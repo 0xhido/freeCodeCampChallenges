58137b2aaed007056a4381409eeee811a45c9c81
mryode <mryodedev@gmail.com>
2019-08-16 17:04:39 +0300

    toggle menu icon

--- ResponsiveWebDesign/ProductLandingPage/main.js
+++ ResponsiveWebDesign/ProductLandingPage/main.js
@@ -1,5 +1,11 @@
 $(document).ready(() => {
     $('.menu-toggle').click(() => {
         $('#nav-bar').toggleClass('active');
+        if ($('#nav-bar').hasClass('active')) {
+            $('.menu-icon').removeClass('fa-bars').addClass('fa-times');
+        }
+        else {
+            $('.menu-icon').removeClass('fa-times').addClass('fa-bars');
+        }
     })
 })
